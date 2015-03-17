3e1952a5fc4754af5ff2ef951e0d0d6a36a71d78
Drew Bollinger <drew@developmentseed.org>
2015-03-17 12:13:46 -0400

    click off to close menu

--- app/assets/javascripts/refills/dropdown.js
+++ app/assets/javascripts/refills/dropdown.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-  $(".dropdown-button").click(function() {
+  $(".dropdown-button").click(function(event) {
     $(".dropdown-menu").toggleClass("show-menu");
     $(".dropdown-menu > li").click(function(){
       $(".dropdown-menu").removeClass("show-menu");
     });
+    event.stopPropagation();
+  });
+  $('body').click(function() {
+    $(".dropdown-menu").removeClass("show-menu");
   });
 });
